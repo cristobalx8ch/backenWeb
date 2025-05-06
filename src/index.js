f1506642f02a42218f8f14b0ae8d5c02ca3154bb
cristobalx8ch <martinezcristobal2005@gmail.com>
2025-05-05 22:25:38 -0400

    config

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const routes = require('./routes/routes.js');
+const morgan = require('morgan');
+const config = require('./config/config.js');
 
 const app = express();
-const port = 3000;
 
+app.set('port', config.app.port);
+app.use(morgan('dev'));
 app.use(routes);
 
 
 
-app.listen(port, () => {
-    console.log(`Servicio levantador: http://localhost:${port}`);
+app.listen(app.get('port'), () => {
+    console.log(`Servicio levantador: http://localhost:${app.get('port')}`);
 });
